92c425d8001e1abf5eadd816adea7e2173c73709
Josh Talks <jst54@cam.ac.uk>
2019-01-21 14:01:08 +0000

    Task 1B attempt

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,31 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
+from utils import sorted_by_key  # noqa
+from haversine import haversine   # formula to calculate distance between two coordinates
+
+def stations_by_distance(stations, p):  
+    station = []   #list station names
+    for station in stations:
+        station.append(stations.name)
+    
+    coordinates = []    #list coordinates
+    for coordinates in stations:
+        coordinates.append(stations.coord)
+
+    distance = []
+    for i in range(len(coordinates)):
+        d = haversine(coordinates[i], p, unit='km')
+        distance.append(d)
+
+    position = []
+    for i in range(len(stations)):
+        position.append((stations[i], distance[i]))
+   
+    return(position)
+
+position.sorted_by_keys()
+
+    
+
+            
