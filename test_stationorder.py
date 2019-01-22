7d8d00ce081c9f8b78b59d599696b01d7da71c0d
Tom Morgan <tmm46@cam.ac.uk>
2019-01-22 20:41:54 +0000

    Added 'passed' note if test is passed

--- test_stationorder.py
+++ test_stationorder.py
@@ -2,3 +2,4 @@ from floodsystem.geo import *
 from floodsystem.stationdata import build_station_list
 
 assert stations_by_distance(build_station_list(), (52.2053, 0.1218))[0] == ('Cambridge Jesus Lock', 0.840237595667494)
+print("passed")
