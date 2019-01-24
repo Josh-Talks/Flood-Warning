e983f51715c2aa47f06bba3b67892c1133e588c4
Tom Morgan <tmm46@cam.ac.uk>
2019-01-24 21:50:56 +0000

    Task1F complete

---
+++ Task1F.py
@@ -0,0 +1,12 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import *
+from floodsystem.station import *
+
+def run():
+    stations = build_station_list()
+    print(inconsistent_typical_range_stations(stations))
+
+
+if __name__ == "__main__":
+    print("*** Task 1F: CUED Part IA Flood Warning System ***")
+    run()
