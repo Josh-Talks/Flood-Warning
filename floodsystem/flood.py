fc84e5875ee2dc032cc36f163bc6dcef9829eee5
Tom Morgan <tmm46@cam.ac.uk>
2019-02-16 14:45:07 +0000

    Added highest relative level function

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -8,3 +8,14 @@ def station_level_over_threshold(stations, tol):
         elif station.relative_water_level() >= tol:
             stations_above_tol.append((station.name, station.relative_water_level()))
     return sorted(stations_above_tol, reverse=True, key=lambda level: level[1])
+
+def stations_highest_rel_level(stations, N):
+    unsorted_stations = []
+    for station in stations:
+        if type(station.relative_water_level()) != float:
+            continue
+        else:
+            unsorted_stations.append((station.name, station.relative_water_level()))
+    stations_by_level = sorted(unsorted_stations, reverse=True, key=lambda level: level[1])
+    first_N = stations_by_level[0:N]
+    return first_N
