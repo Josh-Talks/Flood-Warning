40c592e34eb37ee5df3f672a7e3b9fc82dd336fe
Tom Morgan <tmm46@cam.ac.uk>
2019-02-16 14:08:22 +0000

    Completed Task 2B

---
+++ Task2B.py
@@ -0,0 +1,23 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.station import *
+from floodsystem.flood import *
+
+def run():
+    # Build list of stations
+    stations = build_station_list()
+    
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    stations_at_risk = station_level_over_threshold(stations, 0.8)
+    for i in range(len(stations_at_risk)):
+        print(stations_at_risk[i])
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
+    run()
