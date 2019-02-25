e826978061a28e89a5dd7ee339a690c47ee80f55
Tom Morgan <tmm46@cam.ac.uk>
2019-02-25 11:58:56 +0000

    analysis module added

---
+++ floodsystem/analysis.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib
+from datetime import datetime, timedelta
+from datafetcher import *
+
+def polyfit(dates, level, o):
+    dates
+    x = matplotlib.dates.date2num(dates)
+    y = level
+    p_coeff = np.polyfit(x - x[0], y , 4)
+    poly = np.poly1d(p_coeff)
+    plt.plot(x, y, '.')
+    x1 = np.linspace(x[0], x[-1], 30)
+    plt.plot(x1, poly(x1))
+    plt.show()
