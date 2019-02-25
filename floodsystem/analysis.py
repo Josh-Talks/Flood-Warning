77eb76b8594aeb986039d73c0ca3a09cfb0e46c0
Josh Talks <jst54@cam.ac.uk>
2019-02-25 19:48:36 +0000

    analysis

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -2,14 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from datetime import datetime, timedelta
-from datafetcher import *
 
 def polyfit(dates, levels, p):
-    x = matplotlib.dates.date2num(dates)
-    y = levels
-    p_coeff = np.polyfit(x - x[0], y , 4)
+    dates = matplotlib.dates.date2num(dates)
+    x = np.linspace(max(dates), min(dates), len(dates))
+    d0 = x[0]
+    p_coeff = np.polyfit(x-d0, levels, p)
     poly = np.poly1d(p_coeff)
-    plt.plot(x, y, '.')
-    x1 = np.linspace(x[0], x[-1], 30)
-    plt.plot(x1, poly(x1))
-    plt.show()
+
+    return poly, x
