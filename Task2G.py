6bda5d0a8ce1952c2fd6bc28029c5562e6b73eaa
Tom Morgan <tmm46@cam.ac.uk>
2019-02-26 16:36:57 +0000

    Task2G.py

---
+++ Task2G.py
@@ -0,0 +1,44 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.station import *
+from floodsystem.flood import *
+from floodsystem.plot import *
+from floodsystem.analysis import *
+from floodsystem.datafetcher import *
+
+def run():
+    # Build list of stations
+    stations = build_station_list()
+    
+    # Update latest level data for all stations
+    # update_water_levels(stations)
+    
+    dt = 2
+    p = 4
+    level_tol = 0.9
+    gradient_tol = 0.001
+    
+    at_risk = []
+    for station in stations:
+        if station.relative_water_level == None:
+            continue
+        else:
+            dates, levels = fetch_measure_levels(station.measure_id,dt=datetime.timedelta(days=dt))
+            if polyfit(dates, levels, p) != None:
+                poly, x = polyfit(dates, levels, p)
+                current_grad = current_gradient(poly)
+                if station_at_risk(station, current_grad, level_tol, gradient_tol) == True:
+                    at_risk.append(station.name)
+
+    print(at_risk)
+        
+    
+   
+        
+
+    
+    
+        
+
+if __name__ == "__main__":
+    print("*** Task 2G: CUED Part IA Flood Warning System ***")
+    run()
