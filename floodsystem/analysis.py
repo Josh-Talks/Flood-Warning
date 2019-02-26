d195a129a7f0ad2678f3ef24e1ab3ac20a018deb
Tom Morgan <tmm46@cam.ac.uk>
2019-02-26 16:37:48 +0000

    Added gradient function

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -5,9 +5,17 @@ from datetime import datetime, timedelta
 
 def polyfit(dates, levels, p):
     dates = matplotlib.dates.date2num(dates)
-    x = np.linspace(max(dates), min(dates), len(dates))
-    d0 = x[0]
-    p_coeff = np.polyfit(x-d0, levels, p)
-    poly = np.poly1d(p_coeff)
+    if len(dates) > 1:
+        x = np.linspace(max(dates), min(dates), len(dates))
+        d0 = x[0]
+        p_coeff = np.polyfit(x-d0, levels, p)
+        poly = np.poly1d(p_coeff)
+        return poly, x
+    else:
+        return None
 
-    return poly, x
+def current_gradient(poly):
+    poly = np.asarray(poly)
+    grad = np.gradient(poly)
+    current_grad = float(grad[-1])
+    return current_grad
