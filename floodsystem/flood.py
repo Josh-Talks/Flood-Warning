9b5beabbe27b82ec19e656ef0fde8cc24e854bf5
Tom Morgan <tmm46@cam.ac.uk>
2019-02-26 17:51:29 +0000

    added function that returns the stations as objects if they are over a threshold level

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,4 +1,8 @@
 """Code dealing with flood response"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from datetime import datetime, timedelta
 
 def station_level_over_threshold(stations, tol):
     stations_above_tol = []
@@ -19,3 +23,25 @@ def stations_highest_rel_level(stations, N):
     stations_by_level = sorted(unsorted_stations, reverse=True, key=lambda level: level[1])
     first_N = stations_by_level[0:N]
     return first_N
+
+def station_level_over_thresh(stations, tol):
+    stations_above_tol = []
+    for station in stations:
+        if type(station.relative_water_level()) != float:
+            pass 
+        elif station.relative_water_level() >= tol:
+            stations_above_tol.append(station)
+    return stations_above_tol
+
+def station_at_risk(station, current_grad, level_tol, gradient_tol=0):
+    if type(station.relative_water_level()) != float:
+        return False
+    elif type(current_grad) != float:
+        return False
+    elif current_grad > gradient_tol and station.relative_water_level() > level_tol:
+        return True
+    else:
+        return False
+        
+
+    
