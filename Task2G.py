a3e12876575e4b04539ed8d128ba3c50ac6b1b42
Tom Morgan <tmm46@cam.ac.uk>
2019-02-26 17:50:40 +0000

    More attempts to do this task

--- Task2G.py
+++ Task2G.py
@@ -10,25 +10,20 @@ def run():
     stations = build_station_list()
     
     # Update latest level data for all stations
-    # update_water_levels(stations)
+    update_water_levels(stations)
     
     dt = 2
     p = 4
     level_tol = 0.9
     gradient_tol = 0.001
-    
-    at_risk = []
-    for station in stations:
-        if station.relative_water_level == None:
-            continue
-        else:
-            dates, levels = fetch_measure_levels(station.measure_id,dt=datetime.timedelta(days=dt))
-            if polyfit(dates, levels, p) != None:
-                poly, x = polyfit(dates, levels, p)
-                current_grad = current_gradient(poly)
-                if station_at_risk(station, current_grad, level_tol, gradient_tol) == True:
-                    at_risk.append(station.name)
-
+    at_risk = station_level_over_thresh(stations, level_tol)
+    for station in at_risk:
+        dates, levels = fetch_measure_levels(station.measure_id,dt=datetime.timedelta(days=dt))
+#        if polyfit(dates, levels, p) != None:
+ #           poly, x = polyfit(dates, levels, p)
+  #          current_grad = current_gradient(poly)
+   #         if station_at_risk(station, current_grad, level_tol, gradient_tol) == True:
+    #            at_risk.append(station.name)
     print(at_risk)
         
     
