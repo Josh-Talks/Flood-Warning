4983b5cf75bf975e9c4f40f90b179753d025a362
Tom Morgan <tmm46@cam.ac.uk>
2019-02-27 15:35:54 +0000

    updated test for sample data

--- test_rivers_with_station.py
+++ test_rivers_with_station.py
@@ -1,9 +1,8 @@
-from floodsystem.stationdata import build_station_list
-from floodsystem.geo import *
+from floodsystem.sampledata import build_sample_data
+from floodsystem.geo import rivers_with_station
 
-river_with_station = rivers_with_station(build_station_list())
-river_top_ten = []
-for n in range(10):
-    river_top_ten.append(river_with_station[n])
-assert river_top_ten == ['Addlestone Bourne', 'Aire Washlands', 'Alconbury Brook', 'Aldingbourne Rife', 'Aller Brook', 'Alverthorpe Beck', 'Ampney Brook', 'Amwell Loop', 'Arkle Beck', 'Arrowe Brook']
-print("passed")
+river_with_station = rivers_with_station(build_sample_data())
+river_top_three = []
+for n in range(3):
+    river_top_three.append(river_with_station[n])
+assert river_top_three == ['river1', 'river2', 'river3']
