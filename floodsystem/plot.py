9eaa27ab652b2da66d0484328d629d54d705a2e1
Josh Talks <jst54@cam.ac.uk>
2019-02-27 16:21:53 +0000

    plot

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -12,7 +12,6 @@ def plot_water_levels(station, dates, levels):
     low_level = station.typical_range[0]
     plt.axhline(y=high_level, color='r', linestyle='-', label = "high level")
     plt.axhline(y=low_level, color='g', linestyle='-', label = "low level")
-
     plt.xlabel('date')
     plt.ylabel('water level (m)')
     plt.xticks(rotation=45)
@@ -40,34 +39,3 @@ def plot_water_level_with_fit(station, dates, levels, p):
     plt.legend()
     plt.show()
 
-    """
-    plt.plot(dates, levels, 'b')
-    high_level = station.typical_range[1]
-    low_level = station.typical_range[0]
-    # plt.plot(t, high_level, 'r')
-    # plt.plot(t, low_level, 'b')
-    plt.axhline(y=high_level, color='r', linestyle='-', label = "high level")
-    plt.axhline(y=low_level, color='g', linestyle='-', label = "low level")
-
-    plt.xlabel('date')
-    plt.ylabel('water level (m)')
-    plt.xticks(rotation=45)
-    plt.title(station.name)
-    plt.legend()
-
-    # Display plot
-    plt.tight_layout()  # This makes sure plot does not cut off date labels
-
-    x = matplotlib.dates.date2num(dates)
-    y = levels
-    p_coeff = np.polyfit(x - x[0], y , 4)
-    print(p_coeff)
-    poly = np.poly1d(p_coeff)
-    print(x)
-    print(y)
-    plt.plot(x, y, '.')
-    x1 = np.linspace(x[0], x[-1], 30)
-    plt.plot(x1, poly(x1))
-
-    plt.show()
-    """
