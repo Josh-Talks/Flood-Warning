5313615e4c67877adccc7fb292f8bea5d9282e6b
Tom Morgan <tmm46@cam.ac.uk>
2019-02-27 14:49:39 +0000

    updated stationorder test for the sample data

--- test_stationorder.py
+++ test_stationorder.py
@@ -1,5 +1,5 @@
 from floodsystem.geo import *
 from floodsystem.stationdata import build_station_list
+from floodsystem.sampledata import build_sample_data
 
-assert stations_by_distance(build_station_list(), (52.2053, 0.1218))[0] == ('Cambridge Jesus Lock', 0.840237595667494)
-print("passed")
+assert stations_by_distance(build_sample_data(), (0.0, 0.0)) == [('label1', 157.2495984740402), ('label2', 314.47523947196964), ('label3', 471.65293997288967), ('label4', 628.7586658391518), ('label5', 785.7683061901735)]
