1fb04626f96e134ba9449b3691be4290ae70be5a
Josh Talks <jst54@cam.ac.uk>
2019-02-27 17:22:50 +0000

    test

--- test_rivers_by_station_number.py
+++ test_rivers_by_station_number.py
@@ -1,13 +1,10 @@
-from floodsystem.stationdata import build_station_list
+from floodsystem.sampledata import *
 from floodsystem.geo import *
 
-stations = build_station_list()
+
+stations = build_sample_data()
     
-N = 9
+N = 1
 rivers_by_num_stations = rivers_by_station_number(stations, N)
-for river in rivers_by_num_stations[9:]:
-    if river[1] == rivers_by_num_stations[9][1]:
-        position = rivers_by_num_stations.index(river)
     
-assert(rivers_by_num_stations[:position]) == [('River Thames', 55), ('River Avon', 32), ('River Great Ouse', 31), ('River Aire', 23), ('River Test', 22), ('River Calder', 21), ('River Severn', 21), ('River Derwent', 20), ('River Stour', 18)]
-print("passed")
+assert(rivers_by_num_stations == [('river1', 2), ('river2', 1), ('river3', 1), ('river4', 1)])
